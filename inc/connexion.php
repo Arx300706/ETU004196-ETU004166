de67a7b748921610f5cc44bf32da4cd9b9189295
Arx300706 <Armandonandrasana@gmail.com>
2025-07-14 17:03:33 +0300

    le nouveau

--- inc/connexion.php
+++
@@ -1,6 +0,0 @@
-<?php
-$mysqli = new mysqli('localhost', 'root', '', 'Examenfinal');
-if ($mysqli->connect_errno) {
-    die('Erreur de connexion à la base de données : ' . $mysqli->connect_error);
-}
-?>
